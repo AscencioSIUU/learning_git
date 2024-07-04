03e9a74a0d3316acd0888718c66a10829ebe2357
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-07-04 13:18:30 -0600

    adding comment in Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -9,9 +9,7 @@ public class Calc {
         return n1 - n2;
     }
 
-    // COMENTARIO CREADO EN LA RAMA DE DEV SIUU
-    // GUAPO EL QUE LO LEA
-    // AHHH
+    // comentario en dev
     public int multiplicacion(int n1, int n2) {
         return n1 * n2;
     }
