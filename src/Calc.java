9e54ecd3d0947bddfb655511d8cdf40fb8a99148
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-07-04 13:12:27 -0600

    message in dev

--- src/Calc.java
+++ src/Calc.java
@@ -1,16 +1,16 @@
 package src;
 
-
-
 public class Calc {
-    public int suma(int n1, int n2){
+    public int suma(int n1, int n2) {
         return n1 + n2;
     }
 
-    public int resta(int n1, int n2){
+    public int resta(int n1, int n2) {
         return n1 - n2;
     }
 
+    // COMENTARIO CREADO EN LA RAMA DE DEV SIUU
+    // GUAPO EL QUE LO LEA
     public int multiplicacion(int n1, int n2) {
         return n1 * n2;
     }
