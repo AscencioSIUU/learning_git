6886ff06fbc1c08d7aed1f6539b9caa02cfa5f7b
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-07-04 12:29:13 -0600

    ui done

--- src/UI.java
+++ src/UI.java
@@ -8,8 +8,9 @@ public class UI {
           System.out.println("LA MEJOR CALCU");
           Scanner teclado = new Scanner(System.in);
           int operacion, n1, n2;
-          while (true) {
-               System.out.println("Operacion a realizar:  \n1. Suma \n2. Resta \n3. Multiplicación \n4. Divición");
+          boolean flag = true;
+          while (flag) {
+               System.out.println("Operacion a realizar:  \n1. Suma \n2. Resta \n3. Multiplicación \n4. Salir");
                operacion = teclado.nextInt();
                switch (operacion) {
                     case 1:
@@ -17,7 +18,7 @@ public class UI {
                          n1 = teclado.nextInt();
                          System.out.println("Ingrese Segundo Numero");
                          n2 = teclado.nextInt();
-                         calcu.suma(n1, n2);
+                         System.out.println("El resultado es: " + calcu.suma(n1, n2));
                          break;
 
                     case 2:
@@ -25,7 +26,7 @@ public class UI {
                          n1 = teclado.nextInt();
                          System.out.println("Ingrese Segundo Numero");
                          n2 = teclado.nextInt();
-                         calcu.resta(n1, n2);
+                         System.out.println("El resultado es: " + calcu.resta(n1, n2));
                          break;
 
                     case 3:
@@ -33,7 +34,11 @@ public class UI {
                          n1 = teclado.nextInt();
                          System.out.println("Ingrese Segundo Numero");
                          n2 = teclado.nextInt();
-
+                         System.out.println("El resultado es: " + calcu.multiplicacion(n1, n2));
+                         break;
+                    case 4:
+                         System.out.println("saliendo...");
+                         flag = false;
                          break;
 
                     default:
